00a341d11fcc987c3bfff0f1fbb80c09fc474f51
KJ <884485m@gmail.com>
2020-12-17 12:24:54 +0200

    Update font

--- client/src/components/Cards/cards-styling/card-styling.js
+++ client/src/components/Cards/cards-styling/card-styling.js
@@ -1,11 +1,12 @@
 import styled, { css } from "styled-components";
 import colors from "../../../styles/colors";
+import "../../../styles/fonts.css";
 
 export const StyledCardContainer = css`
   /* p {
     max-height: calc(100vh - 200px - 6em);
   } */
-  background-color: ${colors.teal};
+  background-color: ${colors.mainOrange};
   width: 60vw;
   /* width: 80%; */
   height: ${(props) => props.height};
@@ -15,10 +16,12 @@ export const StyledCardContainer = css`
   box-shadow: 20px 40px ${colors.shadowblack};
   border: 4px solid ${colors.white};
   margin-bottom: 50px;
+  font-family: "Varela Round";
+  font-size: 22px;
 `;
 
 export const StyledHeader = styled.h2`
-  color: ${colors.darkred};
+  color: ${colors.mainBlue};
   text-align: center;
   padding: 0.5em;
 `;
