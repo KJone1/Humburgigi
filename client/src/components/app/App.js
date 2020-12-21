b233a559e2e27fbe302106759ed4a7dc9b9bb0d3
KJ <884485m@gmail.com>
2020-12-21 20:59:15 +0200

    add new peach color bg

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -18,7 +18,8 @@ const Container = styled.div`
   /* background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNSrhbqKsSe-rEL3YYKyUZj9eZ1SxB3_GVDw&usqp=CAU"); */
   /* background-image: url("https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"); */
   background-size: cover;
-  background-image: url("https://mewallpaper.com/thumbnail/abstract/7958-orange-yellow-gradient-background-hq-image-free-wallpaper.jpg");
+  background-image: url("https://lh3.googleusercontent.com/proxy/snjcr0I_DTPxo1so6zajDSMmvxjZa5iYeHCi9-V7HFA7KpAE0IhOhznaU4ol7I6Q2RGhY33I7X60H338D7VpuUXkpGp1X7nKjss9vpuXFoFfXZaMWnlJgU03i4wotrLZ-m7vmeyOjWHoOThtEgw");
+  /* background-image: url("https://mewallpaper.com/thumbnail/abstract/7958-orange-yellow-gradient-background-hq-image-free-wallpaper.jpg"); */
 `;
 
 const App = () => {
